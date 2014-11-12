cdc94265a1d422621959c4e66d731b9433532291
fono09 <fono@fono.jp>
2014-11-12 11:52:21 +0900

    もうこれだけでいいんじゃないかな

---
+++ timer.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main(int argc,char *argv[]){
+
+	if(argc == 0){
+
+		printf("引数ください\n");
+
+	}
+		
+	double n;
+	int sec;
+
+	sscanf(argv[1],"%lf",&n);
+	sec = (int)(n*60);
+
+	if(sec == 0){
+
+		printf("短すぎます\n");
+		return 0;
+
+	}
+
+	sleep(sec);
+
+	
+	printf("時間です\a\n");
+
+	return 0;
+
+}
